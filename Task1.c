1feca5e6db97fb602fae210685ad925050f0daa9
mohanadsalah <mohanadsalah281@gmail.com>
2024-03-04 17:14:14 +0200

    cse211_20p3397

---
+++ Task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fun (void);
+int main()
+{
+    fun ();
+    printf("%d",x);
+    return 0;
+}
+int fun (void)
+{
+    int x;
+    x=10;
+    return 0;
+}
